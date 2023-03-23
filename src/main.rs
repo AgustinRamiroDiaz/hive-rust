64ae3074a8ecdb980036717f1d171be81ce81e96
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2023-03-23 20:15:45 +0000

    Improve piece struct
    Signed-off-by: Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,23 @@ struct Board<'a> {
 
 #[derive(PartialEq, Debug)]
 struct Piece {
+    bug: Bug,
+    color: Color,
+}
 
+#[derive(PartialEq, Debug)]
+enum Bug {
+    Bee,
+    Beetle,
+    Grasshopper,
+    Spider,
+    Ant,
+}
+
+#[derive(PartialEq, Debug)]
+enum Color {
+    Black,
+    White,
 }
 
 type Cell<'a> = Vec<&'a Piece>;
@@ -35,18 +51,24 @@ impl<'a> Board<'a> {
     fn get_cell(&self, y: i8, x: i8) -> Option<Cell> {
         self.cells.get(&Coordinate{x, y}).cloned()
     }
-
 }
 
 #[test]
-fn simple_game() {
+fn simple_board() {
+    use Color::*;
+    use Bug::*;
     let mut board = Board{cells: HashMap::new()};
-    let black_bee = Piece{};
-    let white_bee = Piece{};
+    let black_bee = Piece{bug: Bee, color: Black};
+    let white_bee = Piece{bug: Bee, color: White};
+    let black_beetle = Piece{bug: Beetle, color: Black};
+    let white_beetle = Piece{bug: Beetle, color: White};
+
     board.put_piece(&black_bee, 0, 0);
     board.put_piece(&white_bee, 0, 1);
+    board.put_piece(&black_beetle, 0, 1);
+    board.put_piece(&white_beetle, 0, 1);
 
     assert_eq!(board.get_cell(0, 0), Some(vec![&black_bee]));
-    assert_eq!(board.get_cell(0, 1), Some(vec![&white_bee]));
+    assert_eq!(board.get_cell(0, 1), Some(vec![&white_bee, &black_beetle, &white_beetle]));
     assert_eq!(board.get_cell(10, 1), None);
 }
