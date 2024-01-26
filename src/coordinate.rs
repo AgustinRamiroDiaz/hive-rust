049491d221f7eec9a928d3c716cf2eeae28dea71
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-01-26 08:46:32 -0300

    add new generic hexagonal coordinate system
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, marker::PhantomData};
 
 // Useful guide for understanding hexagonal coordinates: https://www.redblobgames.com/grids/hexagons/#neighbors-axial
 
@@ -47,11 +47,78 @@ impl std::ops::Sub for Coordinate {
     }
 }
 
+pub(crate) trait NewHexagonalCoordinateSystem {
+    type Coord;
+    type Direction;
+
+    fn neighbor_coordinates(&self, from: Self::Coord) -> [Self::Coord; 6];
+
+    // Determine if a piece can slide from one cell to another
+    // Behavior is undefined if the cells are not neighbors
+    fn can_slide(&self, from: Self::Coord, to: Self::Coord, hive: &HashSet<Self::Coord>) -> bool;
+
+    fn relative_neighbors_clockwise(&self) -> [Self::Direction; 6];
+}
+
+struct GenericCoordinateSystem<Coord, Direction> {
+    neighbors: [Direction; 6],
+    _phantom: PhantomData<Coord>,
+}
+
+impl<Coord, Direction> NewHexagonalCoordinateSystem for GenericCoordinateSystem<Coord, Direction>
+where
+    Coord: std::ops::Add
+        + std::ops::Add<Direction, Output = Coord>
+        + PartialEq
+        + Eq
+        + std::hash::Hash
+        + std::ops::Sub<Output = Direction>
+        + Copy,
+    Direction: Copy + PartialEq,
+{
+    type Coord = Coord;
+    type Direction = Direction;
+    fn neighbor_coordinates(&self, from: Self::Coord) -> [Self::Coord; 6] {
+        self.neighbors.map(|delta| from + delta)
+    }
+
+    fn can_slide(
+        &self,
+        from: Self::Coord,
+        to: Self::Coord,
+        occupied: &HashSet<Self::Coord>,
+    ) -> bool {
+        let relative_position = to - from;
+
+        // TODO: remove unwrap
+        let relative_neighbors_position = self
+            .neighbors
+            .iter()
+            .position(|&p| p == relative_position)
+            .unwrap();
+
+        let relative_right_neighbor = self.neighbors[(relative_neighbors_position + 1) % 6];
+        let relative_left_neighbor = self.neighbors[(relative_neighbors_position + 5) % 6];
+
+        let right_neighbor = occupied.get(&(from + relative_right_neighbor));
+
+        let left_neighbor = occupied.get(&(from + relative_left_neighbor));
+
+        return left_neighbor.is_none() || right_neighbor.is_none();
+    }
+
+    fn relative_neighbors_clockwise(&self) -> [Self::Direction; 6] {
+        self.neighbors
+    }
+}
+
 pub(crate) trait HexagonalCoordinateSystem {
     type Coord;
 
     fn neighbor_coordinates(from: Self::Coord) -> [Self::Coord; 6];
 
+    // Determine if a piece can slide from one cell to another
+    // Behavior is undefined if the cells are not neighbors
     fn can_slide(from: Self::Coord, to: Self::Coord, hive: &HashSet<Self::Coord>) -> bool;
 
     fn relative_neighbors_clockwise() -> [Self::Coord; 6];
